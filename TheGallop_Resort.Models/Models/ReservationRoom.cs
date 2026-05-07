bc1ff32729da9b885ab51bf9a01ea2ae14f2bb67
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-07 09:45:12 +0200

    Added ReservationRoom Class

---
+++ TheGallop_Resort.Models/Models/ReservationRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGallop_Resort.Models.Models
+{
+    internal class ReservationRoom
+    {
+        public int ReservationId { get; set; }
+        public int RoomId { get; set; }
+
+        public Reservation Reservation { get; set; }
+        public Room Room { get; set; }
+
+    }
+}
