d4aa81d5a45167cdef1983ac75e9da7b2b7b5656
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-11 10:19:03 +0200

    Updated RoomReservation structure

--- TheGallop_Resort.Models/Models/RoomReservation.cs
+++ TheGallop_Resort.Models/Models/RoomReservation.cs
@@ -10,18 +10,20 @@ namespace TheGallop_Resort.Models.Models
     public class RoomReservation
     {
         public int Id { get; set; }
-        public int RoomId { get; set; }
+
         public int BookingId { get; set; }
+        public Booking Booking { get; set; } = null!;
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
-        public RoomStatus Status { get; set; }
-
-        [Range(1, 100, ErrorMessage = "At least 1 adult is required")]
+        
+        [Range(1, 100)]
         public int Adults { get; set; }
-        [Range(0, 100, ErrorMessage = "Number of children cannot be negative")]
+        [Range(0, 100)]
         public int Children { get; set; }
+        
         public decimal PricePerNight { get; set; }
-        public Booking Booking { get; set; } = null!;
+        
+        public int RoomId { get; set; }
         public Room Room { get; set; } = null!;
     }
 
