c6a29b1147aab7bc97ca9cf1bdbdb934d01eae65
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-11 09:44:59 +0200

    Changed properies in RoomReservation-class.

--- TheGallop_Resort.Models/Models/RoomReservation.cs
+++ TheGallop_Resort.Models/Models/RoomReservation.cs
@@ -1,38 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheGallop_Resort.Models.Models
 {
-    public class Reservation
+    public class RoomReservation
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int RoomId { get; set; }
+        public int BookingId { get; set; }
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
-        public Status? Status { get; set; }
+        public RoomStatus Status { get; set; }
 
         [Range(1, 100, ErrorMessage = "At least 1 adult is required")]
         public int Adults { get; set; }
-        
         [Range(0, 100, ErrorMessage = "Number of children cannot be negative")]
         public int Children { get; set; }
-        public int GuestId { get; set; } 
+        public decimal PricePerNight { get; set; }
+        public Booking Booking { get; set; } = null!;
+        public Room Room { get; set; } = null!;
     }
-    public enum Status
+
+    public enum RoomStatus
     {
-        Bokad,
-        Preliminär,
-        Avbokad,
-        Genomförd
+        Confirmed,
+        Cancelled,
+        Preliminary
     }
-
 }
-
-
-
