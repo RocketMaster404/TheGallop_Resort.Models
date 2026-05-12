e96507f48f4b471fb40949c90bb74a8ada992956
erik ny <erik.nyy@gmail.com>
2026-05-12 14:51:36 +0200

    Added Failsafe to GuestController

---
+++ TheGallop_Resort.Api/Migrations/20260512124514_init2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheGallop_Resort.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomStatus",
+                table: "RoomReservations",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomStatus",
+                table: "RoomReservations");
+        }
+    }
+}
