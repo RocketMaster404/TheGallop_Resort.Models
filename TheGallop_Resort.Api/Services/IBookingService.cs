59b99c9ef5e40e35a6e03ee738de2fd2ce045513
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-14 14:06:20 +0200

    Removed DTO as returntype in UpdateGuestBooking

--- TheGallop_Resort.Api/Services/IBookingService.cs
+++ TheGallop_Resort.Api/Services/IBookingService.cs
@@ -10,7 +10,7 @@ namespace TheGallop_Resort.Api.Services
 
         Task<ServiceResult<GetBookingResponseDTO>> GetBookingByIdAsync(int bookingId);
 
-        Task<ServiceResult<UpdateBookingGuestDTO>> UpdateGuestOnBookingAsync(UpdateBookingGuestDTO update);
+        Task<ServiceResult> UpdateGuestOnBookingAsync(UpdateBookingGuestDTO update);
 
         Task<ServiceResult<Booking>> AddBookingAsync(int guestId);
     }
