ee7b9203af880db3676fbf9754d40624a18b5abc
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-14 02:02:16 +0200

    Created DTOs for Room

---
+++ TheGallop_Resort.Api/DTOs/RoomDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheGallop_Resort.Api.DTOs
+{
+    public record CreateRoomDTO
+    {
+        [Range(1001, 1301, ErrorMessage = "Room number must be between 1001-1301")]
+
+        public int RoomNr { get; init; }
+        public int RoomCategoryId { get; init; }
+    }
+
+    public record RoomInfoDTO(int Id, int RoomNr, int RoomCategoryId);
+}
