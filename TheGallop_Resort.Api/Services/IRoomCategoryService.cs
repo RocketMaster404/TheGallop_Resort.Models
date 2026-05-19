701e6e98ee4e05bc8f7a50084815f603a39973c3
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-19 12:29:04 +0200

    Created IRoomCategoryService

---
+++ TheGallop_Resort.Api/Services/IRoomCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using TheGallop_Resort.Api.DTOs;
+using TheGallop_Resort.Models.Models;
+
+namespace TheGallop_Resort.Api.Services
+{
+    public interface IRoomCategoryService
+    {
+        Task<ServiceResult<RoomCategory>> AddRoomCategoryAsync(CreateRoomCategoryDTO dto);
+        Task<IEnumerable<RoomCategory>> GetAllRoomCategoriesAsync();
+        Task<ServiceResult<RoomCategory>> GetRoomCategoryByIdAsync(int roomCategoryId);
+        Task<ServiceResult> DeleteRoomCategoryAsync(int roomCategoryId);
+
+
+    }
+}
