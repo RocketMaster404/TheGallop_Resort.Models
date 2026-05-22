8f2e4a6fe8006f67c3a32eff1d3b61ceeb0feb4f
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-22 17:11:02 +0200

    Added ValidationTestMethod for updateBookingStatus

--- TheGallop_Resort.Tests/BookingTests/BookingValidatorsTests.cs
+++ TheGallop_Resort.Tests/BookingTests/BookingValidatorsTests.cs
@@ -13,7 +13,7 @@ public class BookingValidatorsTests
     {
         var validator = new UpdateBookingGuestDTOValidator();
 
-        var booking = new UpdateBookingGuestDTO(bookingId: 3, guestId: -3);
+        var booking = new UpdateBookingGuestDTO(3, -3);
 
         var result = validator.TestValidate(booking);
 
@@ -25,10 +25,23 @@ public class BookingValidatorsTests
     {
         var validator = new UpdateBookingGuestDTOValidator();
 
-        var booking = new UpdateBookingGuestDTO(bookingId: -1, guestId: 3);
+        var booking = new UpdateBookingGuestDTO(-1,  3);
 
         var result = validator.TestValidate(booking);
 
         result.ShouldHaveValidationErrorFor(x => x.bookingId);
     }
+
+    [TestMethod]
+    public void UpdateBookingStatus_AnotherStatusThanEnum_ReturnError()
+    {
+        var validator = new UpdateBookingStatusDTOValidator();
+        var booking = new UpdateBookingStatusDTO(BookingId : 1, Status: (Status)99);
+
+        var result = validator.TestValidate(booking);
+
+        result.ShouldHaveValidationErrorFor(x => x.Status);
+    }
+
+
 }
