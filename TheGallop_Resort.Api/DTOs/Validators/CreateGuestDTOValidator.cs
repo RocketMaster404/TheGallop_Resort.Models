e2fc7eb885e8d13fae4d244b90aa8b44a630eb92
erik ny <erik.nyy@gmail.com>
2026-05-22 09:07:23 +0200

    Created validation for names in CreateGuest

--- TheGallop_Resort.Api/DTOs/Validators/CreateGuestDTOValidator.cs
+++ TheGallop_Resort.Api/DTOs/Validators/CreateGuestDTOValidator.cs
@@ -10,13 +10,13 @@ namespace TheGallop_Resort.Api.DTOs.Validators
             RuleFor(g => g.FirstName)
                 .NotEmpty().MinimumLength(2)
                 .MaximumLength(50)
-                .WithMessage("Invalid numver of Characters");
+                .WithMessage("Invalid number of Characters");
 
 
             RuleFor(g => g.LastName).NotEmpty()
                 .MinimumLength(2)
                 .MaximumLength(50)
-                .WithMessage("Invalid numbers of characers");
+                .WithMessage("Invalid numbers of characters");
 
             RuleFor(g => g.Email).NotEmpty().EmailAddress();
 
--- TheGallop_Resort.Tests/GuestTests/GuestValidatorsTests.cs
+++ TheGallop_Resort.Tests/GuestTests/GuestValidatorsTests.cs
@@ -60,4 +60,22 @@ public class GuestValidatorsTests
 
         result.ShouldHaveValidationErrorFor(g => g.FirstName);
     }
+
+    [TestMethod]
+    public void Validate_InvalidLastName_ReturnError()
+    {
+        var validator = new CreateGuestDTOValidator();
+
+        var dto = new CreateGuestDTO()
+        {
+            FirstName = "Test",
+            LastName = "T",
+            Email = "Test@testsson.se",
+            Phone = "0727435550"
+        };
+
+        var result = validator.TestValidate(dto);
+
+        result.ShouldHaveValidationErrorFor(g => g.LastName);
+    }
 }
