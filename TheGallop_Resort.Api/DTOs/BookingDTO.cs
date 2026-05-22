bbc21b60ae0b77b62ac521c04434bf9ead049f53
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-22 12:12:13 +0200

    Added Test To AddBookingAsync

--- TheGallop_Resort.Api/DTOs/BookingDTO.cs
+++ TheGallop_Resort.Api/DTOs/BookingDTO.cs
@@ -9,7 +9,6 @@ namespace TheGallop_Resort.Api.DTOs
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public decimal TotalPrice { get; set; }
-
         public Status Status { get; set; }
         public GuestInfoDTO Guest { get; set; }
         public IEnumerable<GetRoomReservationResponseDTO> RoomReservation { get; set; }
