8e18c4d54c2d236d5b49684edbf91485fc8ba42b
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-22 16:50:11 +0200

    Added rules for UpdateBookingGuestDTOValidator.

---
+++ TheGallop_Resort.Api/DTOs/Validators/UpdateBookingGuestDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace TheGallop_Resort.Api.DTOs.Validators
+{
+    public class UpdateBookingGuestDTOValidator : AbstractValidator<UpdateBookingGuestDTO>
+    {
+        public UpdateBookingGuestDTOValidator()
+        {
+            RuleFor(x => x.guestId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("GuestId can't be negative");
+
+            RuleFor(x => x.bookingId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("BookingId can't be negative");
+        }
+    }
+}
