8a223a452bc229da43690135411f23a71cd5ca30
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-05-22 17:10:32 +0200

    Added rules for updateBookingStatusDTOValidator.

---
+++ TheGallop_Resort.Api/DTOs/Validators/UpdateBookingStatusDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace TheGallop_Resort.Api.DTOs.Validators
+{
+    public class UpdateBookingStatusDTOValidator : AbstractValidator<UpdateBookingStatusDTO>
+    {
+        public UpdateBookingStatusDTOValidator()
+        {
+            RuleFor(b => b.BookingId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("BookingId can't be negative");
+
+            RuleFor(b => b.Status)
+                .IsInEnum()
+                .WithMessage("Invalid booking status");
+        }
+    }
+}
