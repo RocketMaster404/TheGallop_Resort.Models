546f6cb3172fb2c11766d6b0e8ab0cef461281de
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-27 13:14:46 +0200

    Refactored RoomDTO for RoomDetailDTO

--- TheGallop_Resort.Api/DTOs/RoomDetailDTO.cs
+++ TheGallop_Resort.Api/DTOs/RoomDetailDTO.cs
@@ -2,13 +2,26 @@
 
 namespace TheGallop_Resort.Api.DTOs
 {
-    public record CreateRoomDTO
+    public record CreateRoomDetailDTO
     {
         // Add this validation into a Validator like Erik's
         [Range(1001, 1301, ErrorMessage = "Room number must be between 1001-1301")]
 
         public int RoomNr { get; init; }
         public int RoomCategoryId { get; init; }
+    }
+
+    public record ReadRoomDetailDTO
+    {
+
+    }
+    public record UpdateRoomDetailDTO
+    {
+
+    }
+    public record DeleteRoomDetailDTO
+    {
+
     }
 
     public record RoomInfoDTO(int Id, int RoomNr, int RoomCategoryId);
