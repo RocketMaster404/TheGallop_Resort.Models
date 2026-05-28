862931838cdc0f000feeed9d7b148021f4606c48
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-28 13:23:19 +0200

    Get out

---
+++ TheGallop_Resort.Api/DTOs/RoomDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheGallop_Resort.Api.DTOs
+{
+    public record CreateRoomDTO
+    {
+        // Add this validation into a Validator like Erik's
+        [Range(1001, 1301, ErrorMessage = "Room number must be between 1001-1301")]
+
+        public int RoomNr { get; init; }
+        public int RoomCategoryId { get; init; }
+    }
+
+    public record RoomInfoDTO(int Id, int RoomNr, int RoomCategoryId);
+}
