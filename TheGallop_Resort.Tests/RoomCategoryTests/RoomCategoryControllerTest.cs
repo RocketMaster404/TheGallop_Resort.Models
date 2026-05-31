b3b2bce982d6dea6499b7db25a898e92b744d65d
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-05-31 08:14:40 +0200

    Added test for DeleteRoomCategory

--- TheGallop_Resort.Tests/RoomCategoryTests/RoomCategoryControllerTest.cs
+++ TheGallop_Resort.Tests/RoomCategoryTests/RoomCategoryControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TheGallop_Resort.Api.Controllers;
 using TheGallop_Resort.Api.DTOs;
 using TheGallop_Resort.Api.Services;
@@ -11,47 +12,47 @@ namespace TheGallop_Resort.Tests;
 [TestClass]
 public class RoomCategoryControllerTest
 {
-
     [TestMethod]
-public async Task AddRoomCategory_AddValidRoomCategory_Return200()
-{
-    var fake = A.Fake<IRoomCategoryService>();
-    var controller = new RoomCategoryController(fake);
-
-    var roomCategoryDto = new RoomCategoryDTO
+    public async Task AddRoomCategory_AddValidRoomCategory_Return200()
     {
-        Type = RoomType.DoubleBed,
-        CategoryPrice = 1800,
-        RoomDetailId = 1
-    };
+        var fake = A.Fake<IRoomCategoryService>();
+        var controller = new RoomCategoryController(fake);
 
-    var roomCategory = new RoomCategory
-    {
-        Id = 1,
-        Type = roomCategoryDto.Type,
-        CategoryPrice = roomCategoryDto.CategoryPrice,
-        RoomDetailId = roomCategoryDto.RoomDetailId
-    };
+        var roomCategoryDto = new RoomCategoryDTO
+        {
+            Type = RoomType.DoubleBed,
+            CategoryPrice = 1800,
+            RoomDetailId = 1
+        };
 
-    A.CallTo(() => fake.AddRoomCategoryAsync(A<RoomCategoryDTO>._))
-        .Returns(ServiceResult<RoomCategory>.Ok(roomCategory));
+        var roomCategory = new RoomCategory
+        {
+            Id = 1,
+            Type = roomCategoryDto.Type,
+            CategoryPrice = roomCategoryDto.CategoryPrice,
+            RoomDetailId = roomCategoryDto.RoomDetailId
+        };
 
-    IActionResult result = await controller.AddRoomCategory(roomCategoryDto);
+        A.CallTo(() => fake.AddRoomCategoryAsync(A<RoomCategoryDTO>._))
+            .Returns(ServiceResult<RoomCategory>.Ok(roomCategory));
 
-    var okResult = result.Should()
-        .BeAssignableTo<OkObjectResult>()
-        .Subject;
+        IActionResult result = await controller.AddRoomCategory(roomCategoryDto);
 
-    var returnedRoomCategory = okResult.Value.Should()
-        .BeAssignableTo<RoomCategory>()
-        .Subject;
-    returnedRoomCategory.Type.Should().Be(roomCategoryDto.Type);
-    returnedRoomCategory.CategoryPrice.Should().Be(roomCategoryDto.CategoryPrice);
-    returnedRoomCategory.RoomDetailId.Should().Be(roomCategoryDto.RoomDetailId);
+        var okResult = result.Should()
+            .BeAssignableTo<OkObjectResult>()
+            .Subject;
 
-    A.CallTo(() => fake.AddRoomCategoryAsync(A<RoomCategoryDTO>._))
-        .MustHaveHappenedOnceExactly();
-}
+        var returnedRoomCategory = okResult.Value.Should()
+            .BeAssignableTo<RoomCategory>()
+            .Subject;
+
+        returnedRoomCategory.Type.Should().Be(roomCategoryDto.Type);
+        returnedRoomCategory.CategoryPrice.Should().Be(roomCategoryDto.CategoryPrice);
+        returnedRoomCategory.RoomDetailId.Should().Be(roomCategoryDto.RoomDetailId);
+
+        A.CallTo(() => fake.AddRoomCategoryAsync(A<RoomCategoryDTO>._))
+            .MustHaveHappenedOnceExactly();
+    }
 
     [TestMethod]
     public async Task GetRoomCategoryById_CheckIfIdExists_ReturnOk()
@@ -90,7 +91,7 @@ public async Task AddRoomCategory_AddValidRoomCategory_Return200()
     }
 
     [TestMethod]
-    public async Task UpdateRoomCategoryAsync_UpdateValidRoomCategory_ReturnNoContent()
+    public async Task UpdateRoomCategory_UpdateValidRoomCategory_ReturnNoContent()
     {
         var fake = A.Fake<IRoomCategoryService>();
         var controller = new RoomCategoryController(fake);
@@ -110,6 +111,23 @@ public async Task AddRoomCategory_AddValidRoomCategory_Return200()
         result.Should().BeAssignableTo<NoContentResult>();
 
         A.CallTo(() => fake.UpdateRoomCategoryAsync(1, roomCategoryDto))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [TestMethod]
+    public async Task DeleteRoomCategory_ExistingId_ReturnsNoContent()
+    {
+        var fake = A.Fake<IRoomCategoryService>();
+        var controller = new RoomCategoryController(fake);
+
+        A.CallTo(() => fake.DeleteRoomCategoryAsync(1))
+            .Returns(ServiceResult.Ok());
+
+        IActionResult result = await controller.DeleteRoomCategory(1);
+
+        result.Should().BeAssignableTo<NoContentResult>();
+
+        A.CallTo(() => fake.DeleteRoomCategoryAsync(1))
             .MustHaveHappenedOnceExactly();
     }
 }
