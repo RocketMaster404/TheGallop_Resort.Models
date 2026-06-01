e1402ffe02b6fffb85b5dbe1b6b892c4178ad2bb
erik ny <erik.nyy@gmail.com>
2026-06-01 15:27:49 +0200

    added DTO GetRoomReservationWithRoomNumberResponseDTO

--- TheGallop_Resort.Api/DTOs/RoomReservationDTO.cs
+++ TheGallop_Resort.Api/DTOs/RoomReservationDTO.cs
@@ -4,7 +4,7 @@ namespace TheGallop_Resort.Api.DTOs
 {
     //Ändra till room-dto ist för int roomId??
     public record GetRoomReservationResponseDTO(int Id, int RoomId, DateTime CheckIn, DateTime CheckOut);
-
+    public record GetRoomReservatioWithRoomNumberResponseDTO(int Id, int RoomId,int RoomNumber, DateTime CheckIn, DateTime CheckOut);
     public record CreateRoomReservationDTO (int bookingId, DateTime CheckIn, DateTime CheckOut, int Adults, int Children, RoomType Type );
 
     public record GetFullRoomReservationResponse(int Id, RoomType Type, DateOnly CheckIn, DateOnly CheckOut, int RoomNr, int Adults,int Children, decimal PricePerNight);
