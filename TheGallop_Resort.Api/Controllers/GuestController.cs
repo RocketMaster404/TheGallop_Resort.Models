5fab72225c1cc0c8741ed9ee1a70e0ae7f21bde2
erik ny <erik.nyy@gmail.com>
2026-06-02 20:47:53 +0200

    Changed response in add guest which lead to changes in testmethod (changed response type)

--- TheGallop_Resort.Api/Controllers/GuestController.cs
+++ TheGallop_Resort.Api/Controllers/GuestController.cs
@@ -37,8 +37,8 @@ namespace TheGallop_Resort.Api.Controllers
             var guest = await _guestService.GetGuestBookingHistoryAsync(guestId);
 
             if(!guest.SuccessfulResult)
-            {
-                return BadRequest(guest.ErrorMessage);
+            {              
+                return NotFound(guest.ErrorMessage);
             }
 
             return Ok(guest.Data);
@@ -50,36 +50,32 @@ namespace TheGallop_Resort.Api.Controllers
             var guest = await _guestService.GetGuestFutureBookingsAsync(guestId);
             if (!guest.SuccessfulResult)
             {
-                return BadRequest(guest.ErrorMessage);
+                return NotFound(guest.ErrorMessage);
             }
 
             return Ok(guest.Data);
         }
 
         [HttpGet]
-        public async Task<ActionResult<Guest>> GetAllGuestsInfo()
+        public async Task<ActionResult<List<GuestInfoDTO>>> GetAllGuestsInfo()
         {
             var guests = await _guestService.GetAllGuestsInfoAsync();
-
-
 
             return Ok(guests);
         }
 
-       
+
 
 
         [HttpPost]
-        public async Task<IActionResult>AddGuest(CreateGuestDTO dto)
+        public async Task<ActionResult<Guest>> AddGuest(CreateGuestDTO dto)
         {
-
             var validation = await _createGuestValidator.ValidateAsync(dto);
 
             if (!validation.IsValid)
             {
                 return BadRequest(validation.Errors);
             }
-
 
             var guest = await _guestService.AddGuestAsync(dto);
 
@@ -88,12 +84,13 @@ namespace TheGallop_Resort.Api.Controllers
                 return BadRequest(guest.ErrorMessage);
             }
 
-            return Ok(guest.Data);
-            
-
+            return CreatedAtAction(
+                nameof(GetGuestInfoById),
+                new { guestId = guest.Data.Id },
+                guest.Data);
         }
 
-     
+
         [HttpGet("{guestId}")]
         public async Task<ActionResult<GuestInfoWithBookingDTO>> GetGuestInfoById(int guestId)
         {
