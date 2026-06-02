332b9c4cc5dfab8e6bead803edcff4436461e898
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-06-02 10:58:01 +0200

    Fixed error

--- TheGallop_Resort.Api/DTOs/Validators/CreateRoomReservationDTOValidator.cs
+++ TheGallop_Resort.Api/DTOs/Validators/CreateRoomReservationDTOValidator.cs
@@ -4,9 +4,9 @@ namespace TheGallop_Resort.Api.DTOs.Validators
 {
     public class CreateRoomReservationDTOValidator : AbstractValidator<CreateRoomReservationDTO>
     {
-        public CreateRoomReservationDTO()
-        {
+        //public CreateRoomReservationDTO()
+        //{
 
-        }
+        //}
     }
 }
