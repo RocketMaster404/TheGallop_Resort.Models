c7d77d1a8a0cc1133d8759898cd63acb4185cfc5
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-06-02 09:46:44 +0200

    Added roomReservationControler

---
+++ TheGallop_Resort.Api/Controllers/RoomReservationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheGallop_Resort.Api.DTOs;
+using TheGallop_Resort.Api.Services;
+
+namespace TheGallop_Resort.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomReservationController : BaseController
+    {
+        private readonly IRoomReservationService _roomReservationService;
+
+        public RoomReservationController(IRoomReservationService roomReservationService)
+        {
+            _roomReservationService = roomReservationService;
+        }
+
+        [HttpPost("CreateRoomReservation", Name = "CreateRoomReservation")]
+        public async Task<ServiceResult<CreateRoomReservationDTO>> CreateRoomReservation(CreateRoomReservationDTO dto)
+        {
+            //var validation = await _.ValidateAsync(dto);
+
+            //if (!validation.IsValid)
+            //{
+            //    return BadRequest();
+            //}
+
+            var result = await _roomReservationService.CreateRoomReservationAsync(dto);
+
+            if (!result.SuccessfulResult)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
