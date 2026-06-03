6efb68d43818ab0fea4e71fdc2bb63bc17ce25f0
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-06-03 09:21:13 +0200

    Added controller for room endpoint

---
+++ TheGallop_Resort.Api/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheGallop_Resort.Api.DTOs;
+using TheGallop_Resort.Api.Services;
+using TheGallop_Resort.Models.Models;
+
+namespace TheGallop_Resort.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : BaseController
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet("getAllRooms", Name = "getAllRooms")]
+        public async Task<ActionResult<IEnumerable<GetRoomResponseDTO>>> GetAllRooms()
+        {
+            var rooms = await _roomService.GetAllRoomsAsync();
+
+            return Ok(rooms.Data);
+        }
+    }
+}
